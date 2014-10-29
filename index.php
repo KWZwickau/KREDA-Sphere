96dc438d6b9a58ab390871c7d1b49d68cebfaf30
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-10-29 23:05:55 +0100

    Research: Client Gui

--- index.php
+++ index.php
@@ -1,105 +1,38 @@
-<!doctype html>
-<html>
-<head>
-    <title>KREDA Professional</title>
-    <link rel="stylesheet" href="Library/Bootstrap/3.2.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="Library/Bootflat/2.0.4/bootflat/css/bootflat.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+<?php
+namespace KREDA\Sphere;
 
-<body>
+use MOC\V\Component\Router\Component\Bridge\Repository\UniversalRouter;
+use MOC\V\Component\Router\Component\Parameter\Repository\RouteParameter;
+use MOC\V\Component\Template\Template;
+use MOC\V\Core\AutoLoader\AutoLoader;
+use MOC\V\Core\HttpKernel\HttpKernel;
 
-<!-- Sphere Navigation -->
-<nav class="navbar navbar-inverse" role="navigation"
-     style="border-radius: inherit; margin: inherit; border-bottom: 1px solid #DDD;">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-3">
-                <div class="navbar-header">
-                    <div class="navbar-text">
-                        KREDA Professional
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-9">
-                <!--                <div id="SphereNavigationCollapse" class="collapse navbar-collapse">-->
-                <ul class="nav navbar-nav">
-                    <li><a href="#"><span class="glyphicon glyphicon-th"></span> Klassen</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-stats"></span> Zensuren</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Grunddaten</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-wrench"></span> Verwaltung</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-question-sign"></span> Hilfe</a></li>
-                    <li><a href="#">Login</a></li>
-                </ul>
-                <!--                </div>-->
-                <!--                <button type="button" data-target="#SphereNavigationCollapse" data-toggle="collapse"-->
-                <!--                        class="navbar-toggle">-->
-                <!--                    <span class="sr-only">Toggle navigation</span>-->
-                <!--                    <span class="icon-bar"></span>-->
-                <!--                    <span class="icon-bar"></span>-->
-                <!--                    <span class="icon-bar"></span>-->
-                <!--                </button>-->
-            </div>
-        </div>
-    </div>
-</nav>
+require_once(__DIR__ . '/Library/MOC-Framework-Mark-V/Core/AutoLoader/AutoLoader.php');
+AutoLoader::getNamespaceAutoLoader('MOC\V', __DIR__ . '/Library/MOC-Framework-Mark-V/');
 
-<!-- Module Navigation -->
-<nav class="navbar" role="navigation" style="border-radius: inherit; margin: inherit; border-bottom: 1px solid #DDD;">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-3">
-                <div class="navbar-header">
-                    <div class="navbar-text">
-                        Zeugnisse
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-9">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Messages</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</nav>
+//$Router = new UniversalRouter();
+//$Router->addRoute(new RouteParameter('/', 'KREDA\Sphere\Application\Gatekeeper\Controller::Method'));
 
-<!-- Application Navigation -->
-<nav class="navbar" role="navigation" style="border-radius: inherit; margin: inherit; border-bottom: 1px solid #DDD;">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12">
-
-            </div>
-        </div>
-    </div>
-</nav>
-
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-xs-12">
-            <h1>Anmeldung</h1>
-            <div class="h3">Anmeldung</div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12">
-
-        </div>
-    </div>
-</div>
-
-<!-- Bootstrap -->
-<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="Library/Bootstrap/3.2.0/dist/js/bootstrap.min.js"></script>
-
-<!-- Bootflat's JS files.-->
-<script src="Library/Bootflat/2.0.4/bootflat/js/icheck.min.js"></script>
-<script src="Library/Bootflat/2.0.4/bootflat/js/jquery.fs.selecter.min.js"></script>
-<script src="Library/Bootflat/2.0.4/bootflat/js/jquery.fs.stepper.min.js"></script>
-
-</body>
-</html>
-
-
+$TemplateMain = Template::getTemplate(__DIR__ . '/Sphere/Client/Template/Main.twig');
+$TemplateMain->setVariable('MainBody',
+    Template::getTemplate(__DIR__ . '/Sphere/Client/Template/Navigation/Level-0.twig')
+        ->getContent()
+  //  . Template::getTemplate(__DIR__ . '/Sphere/Client/Template/Navigation/Level-1.twig')
+  //      ->getContent()
+    . Template::getTemplate(__DIR__ . '/Sphere/Client/Template/Navigation/Level-2.twig')
+        ->getContent()
+    //. $Router->getRoute()
+);
+ob_start();
+var_dump(
+    HttpKernel::getRequest()->getPathBase(),
+    HttpKernel::getRequest()->getUrlBase(),
+    HttpKernel::getRequest()->getPathInfo(),
+    HttpKernel::getRequest()->getPort(),
+    HttpKernel::getRequest()->getParameterArray()
+)    ;
+$Foot = ob_get_clean();
+$TemplateMain->setVariable( 'MainFoot',
+    $Foot
+);
+print $TemplateMain->getContent();
