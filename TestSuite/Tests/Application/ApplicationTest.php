478aee4e7db6e485a0c37fd0530c5a7491eb9b59
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-11-24 19:19:47 +0100

    Code Review: TestSuite

---
+++ TestSuite/Tests/Application/ApplicationTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace KREDA\TestSuite\Tests\Application;
+
+/**
+ * Class ApplicationTest
+ *
+ * @package KREDA\TestSuite\Tests\Application
+ */
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testAbstractClass()
+    {
+
+    }
+
+}
---
+++ TestSuite/Tests/Application/Gatekeeper/Service/TokenTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace KREDA\TestSuite\Tests\Application\Gatekeeper\Service;
+
+/**
+ * Class TokenTest
+ *
+ * @package KREDA\TestSuite\Tests\Application\Gatekeeper\Service
+ */
+class TokenTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstructor()
+    {
+
+    }
+
+}
