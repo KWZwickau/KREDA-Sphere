632fc7fa1db5611782c475e145c3e2825a623566
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2015-05-27 16:33:48 +0200

    Code Review: Refactor/Quality

--- Sphere/Client/Client.php
+++ Sphere/Client/Client.php
@@ -2,6 +2,7 @@
 namespace KREDA\Sphere\Client;
 
 use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\ORMException;
 use KREDA\Sphere\Application\Assistance\Assistance;
 use KREDA\Sphere\Application\Billing\Billing;
 use KREDA\Sphere\Application\Gatekeeper\Gatekeeper;
@@ -87,6 +88,14 @@ class Client
                      */
                     $this->Display->extensionDebugger()->addProtocol( $Exception->getMessage(), 'warning-sign' );
                     $this->Display->addToContent( new Container( Database::stageRepair( $Exception ) ) );
+                } catch( ORMException $Exception ) {
+                    /**
+                     * Repair Database
+                     */
+                    $this->Display->extensionDebugger()->addProtocol( $Exception->getMessage(), 'warning-sign' );
+                    /** @var Element $Route */
+                    $Route = $this->Configuration->getClientRouter()->getRoute( '/Sphere/Assistance/Support/Application/Missing' );
+                    $this->Display->addToContent( new Container( $Route ) );
                 } catch( DatabaseException $Exception ) {
                     /**
                      * Error
