733e50f782c4b4e77459063049cd8d3bc934e764
Johannes <hannesk001@web.de>
2015-06-24 14:27:14 +0200

    Management Contact basic

--- Sphere/Application/Management/Service/Contact.php
+++ Sphere/Application/Management/Service/Contact.php
@@ -1,9 +1,13 @@
 <?php
 namespace KREDA\Sphere\Application\Management\Service;
 
+use KREDA\Sphere\Application\Management\Service\Company\Entity\TblCompany;
 use KREDA\Sphere\Application\Management\Service\Contact\Entity\TblContact;
+use KREDA\Sphere\Application\Management\Service\Contact\Entity\TblMail;
+use KREDA\Sphere\Application\Management\Service\Contact\Entity\TblPhone;
 use KREDA\Sphere\Application\Management\Service\Contact\EntityAction;
 use KREDA\Sphere\Application\Management\Service\Person\Entity\TblPerson;
+use KREDA\Sphere\Application\System\System;
 use KREDA\Sphere\Common\Database\Handler;
 
 /**
@@ -45,14 +49,180 @@ class Contact extends EntityAction
     }
 
     /**
-     * @param TblPerson $tblPerson
-     *
-     * @return bool|TblContact[]
+     * @return bool|Contact\Entity\TblContact[]
      */
-    public function entityContactAllByPerson( TblPerson $tblPerson )
+    public function entityContactAll()
     {
-
-        return parent::entityContactAllByPerson( $tblPerson );
+        return parent::entityContactAll();
     }
 
+    /**
+     * @param int $Id
+     *
+     * @return bool|TblPhone
+     */
+    public function entityPhoneById($Id)
+    {
+        return parent::entityPhoneById($Id); 
+    }
+
+    /**
+     * @param TblPerson $tblPerson
+     *
+     * @return bool|Contact\Entity\TblPhone[]
+     */
+    public function entityPhoneAllByPerson(TblPerson $tblPerson)
+    {
+        return parent::entityPhoneAllByPerson($tblPerson); 
+    }
+
+    /**
+     * @param TblCompany $tblCompany
+     *
+     * @return bool|Contact\Entity\TblPhone[]
+     */
+    public function entityPhoneAllByCompany(TblCompany $tblCompany)
+    {
+        return parent::entityPhoneAllByCompany($tblCompany); 
+    }
+
+    /**
+     * @param int $Id
+     *
+     * @return bool|TblMail
+     */
+    public function entityMailById($Id)
+    {
+        return parent::entityMailById($Id); 
+    }
+
+    /**
+     * @param TblPerson $tblPerson
+     *
+     * @return bool|Contact\Entity\TblMail[]
+     */
+    public function entityMailAllByPerson(TblPerson $tblPerson)
+    {
+        return parent::entityMailAllByPerson($tblPerson); 
+    }
+
+    /**
+     * @param TblCompany $tblCompany
+     *
+     * @return bool|Contact\Entity\TblMail[]
+     */
+    public function entityMailAllByCompany(TblCompany $tblCompany)
+    {
+        return parent::entityMailAllByCompany($tblCompany); 
+    }
+
+    /**
+     * @param $Name
+     * @param $Description
+     *
+     * @return TblContact|null
+     */
+    public function actionCreateContact(
+        $Name,
+        $Description
+    )
+    {
+        return parent::actionCreateContact($Name, $Description); 
+    }
+
+    /**
+     * @param TblPerson $tblPerson
+     * @param TblContact $tblContact
+     * @param $Address
+     * @param $Description
+     *
+     * @return TblMail|null
+     */
+    public function actionCreatePersonMail(
+        TblPerson $tblPerson,
+        TblContact $tblContact,
+        $Address,
+        $Description
+    )
+    {
+        return parent::actionCreatePersonMail($tblPerson, $tblContact, $Address, $Description); 
+    }
+
+    /**
+     * @param TblPerson $tblPerson
+     * @param TblContact $tblContact
+     * @param $Number
+     * @param $Description
+     *
+     * @return TblPhone|null
+     */
+    public function actionCreatePersonPhone(
+        TblPerson $tblPerson,
+        TblContact $tblContact,
+        $Number,
+        $Description
+    )
+    {
+        return parent::actionCreatePersonPhone($tblPerson, $tblContact, $Number, $Description); 
+    }
+
+    /**
+     * @param TblCompany $tblCompany
+     * @param TblContact $tblContact
+     * @param $Number
+     * @param $Description
+     *
+     * @return TblPhone|null
+     */
+    public function actionCreateCompanyPhone(
+        TblCompany $tblCompany,
+        TblContact $tblContact,
+        $Number,
+        $Description
+    )
+    {
+        return parent::actionCreateCompanyPhone($tblCompany, $tblContact, $Number, $Description); 
+    }
+
+    /**
+     * @param TblCompany $tblCompany
+     * @param TblContact $tblContact
+     * @param $Address
+     * @param $Description
+     *
+     * @return TblMail|null
+     */
+    public function actionCreateCompanyMail(
+        TblCompany $tblCompany,
+        TblContact $tblContact,
+        $Address,
+        $Description
+    )
+    {
+        return parent::actionCreateCompanyMail($tblCompany, $tblContact, $Address, $Description); 
+    }
+
+    /**
+     * @param TblMail $tblMail
+     *
+     * @return bool
+     */
+    public function actionDestroyMail(
+        TblMail $tblMail
+    )
+    {
+        return parent::actionDestroyMail($tblMail); 
+    }
+
+    /**
+     * @param TblPhone $tblPhone
+     *
+     * @return bool
+     */
+    public function actionDestroyPhone(
+        TblPhone $tblPhone
+    )
+    {
+        return parent::actionDestroyPhone($tblPhone); 
+    }
 }
